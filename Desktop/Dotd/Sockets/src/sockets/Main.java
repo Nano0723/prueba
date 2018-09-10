fe0db1ba368e8de8a12487bc8c6a0fe825f4dcbb
Jose Ignacio <nano_masis@hotmail.com>
2018-09-09 23:35:38 -0600

    servidor socket

---
+++ Desktop/Dotd/Sockets/src/sockets/Main.java
@@ -0,0 +1,17 @@
+
+package sockets;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Conector c =new Conector();
+		c.iniciar();
+		cliente c1=new cliente();
+		c1.inicio();
+		
+	}
+
+}
